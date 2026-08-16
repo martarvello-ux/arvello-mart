dcd2cffcda83908a2dfcceda92ce8fc491d39275
martarvello-ux <martarvello@gmail.com>
2026-08-16 19:21:33 +0600

    Update products.js

--- products.js
+++ products.js
@@ -1,32 +1,47 @@
-const products = [
-  {
-    cat: "Watches",
-    images: [
-      "poedagar-930-1.jpg",
-      "poedagar-930-2.jpg"
-    ],
-    bn: "POEDAGAR 930 | Silver Green",
-    en: "POEDAGAR 930 | Silver Green",
-    price: "৳ 950"
-  },
+const products = [];
 
-  {
-    cat: "Clothing",
-    images: [
-      "premium-polo-striped-shirt.jpg"
-    ],
-    bn: "Premium POLO Striped Shirt",
-    en: "Premium POLO Striped Shirt",
-    price: "৳ 650"
-  },
 
-  {
-    cat: "Shoes",
-    images: [
-      "chunky-casual-sneaker.jpg"
-    ],
-    bn: "Chunky Casual Sneaker",
-    en: "Chunky Casual Sneaker",
-    price: "৳ 1,790"
-  }
-];
+/* =========================
+   PRODUCT 1
+========================= */
+
+products.push({
+  cat: "Watches",
+  images: [
+    "poedagar-930-1.jpg",
+    "poedagar-930-2.jpg"
+  ],
+  bn: "POEDAGAR 930 | Silver Green",
+  en: "POEDAGAR 930 | Silver Green",
+  price: "৳ 950"
+});
+
+
+/* =========================
+   PRODUCT 2
+========================= */
+
+products.push({
+  cat: "Clothing",
+  images: [
+    "premium-polo-striped-shirt.jpg"
+  ],
+  bn: "Premium POLO Striped Shirt",
+  en: "Premium POLO Striped Shirt",
+  price: "৳ 650"
+});
+
+
+/* =========================
+   PRODUCT 3
+========================= */
+
+products.push({
+  cat: "Shoes",
+  images: [
+    "chunky-casual-sneaker.jpg"
+  ],
+  bn: "Chunky Casual Sneaker",
+  en: "Chunky Casual Sneaker",
+  price: "৳ 1,790"
+});
