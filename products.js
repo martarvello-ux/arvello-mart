28b4f38877ef23a9560c0490b79813f7f0dfbdf7
martarvello-ux <martarvello@gmail.com>
2026-08-16 18:49:57 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    cat: "Watches",
+    images: [
+      "poedagar-930-1.jpg",
+      "poedagar-930-2.jpg"
+    ],
+    bn: "POEDAGAR 930 | Silver Green",
+    en: "POEDAGAR 930 | Silver Green",
+    price: "৳ 950"
+  },
+
+  {
+    cat: "Clothing",
+    images: [
+      "premium-polo-striped-shirt.jpg"
+    ],
+    bn: "Premium POLO Striped Shirt",
+    en: "Premium POLO Striped Shirt",
+    price: "৳ 650"
+  },
+
+  {
+    cat: "Shoes",
+    images: [
+      "chunky-casual-sneaker.jpg"
+    ],
+    bn: "Chunky Casual Sneaker",
+    en: "Chunky Casual Sneaker",
+    price: "৳ 1,790"
+  }
+];
